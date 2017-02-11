c7aa1d99b41f0a437fb1ca09cfe928a808d71ca7
zhougynui@sina.com <qwertyu8>
2017-02-11 20:11:34 +0800

    a

---
+++ src/IMS.Web/Controllers/Failure/FailureController.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IMS.Web.Controllers.Failure
+{
+    public class FailureController : Controller
+    {
+        //
+        // GET: /Failure/
+        
+         public ActionResult FailureReport()
+        {
+            return View();
+
+        }
+
+        
+	}
+}
---
+++ src/IMS.Web/Controllers/Partial/PartialController.cs
@@ -0,0 +1,35 @@
+﻿using IMS.Data;
+using IMS.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IMS.Web.Controllers.Partial
+{
+    public class PartialController : Controller
+    {
+        //
+        // GET: /Partial/
+        private SubSystemRepository subSystemRepository = new SubSystemRepository();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult SubSystem() 
+        {
+            return PartialView();
+        }
+        public ActionResult GetSubSystemList(int lev, int parentId) 
+        {
+            subSystemRepository.GetList(new { Lev = 0 });
+            IEnumerable<SubSystem> subSystemList = subSystemRepository.GetSubSystemList(lev,parentId);
+            if (subSystemList!=null)
+            {
+                return Json(subSystemList);
+            }
+            return Json(subSystemList);
+        }
+	}
+}
