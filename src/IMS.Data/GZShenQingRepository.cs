f9d600163300b85748b328a6bb867f832469e64a
zhougynui@sina.com <qwertyu8>
2017-02-19 19:50:07 +0800

    ff

---
+++ src/IMS.Data/GZShenQingRepository.cs
@@ -0,0 +1,57 @@
+﻿using IMS.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMS.Data
+{
+    public class GZShenQingRepository : RepositoryBase<GZShenQing>
+    {
+        SBXXRespository SBXXRespository = new SBXXRespository();
+
+        public IEnumerable<SBXX> GetShortNameList()
+        {
+            try
+            {
+                IEnumerable<SBXX> ShortNameList = SBXXRespository.GetList();
+                return ShortNameList;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+
+        public bool AddNewFailure(GZShenQing GZTiJiao)
+        {
+            try
+            {
+                bool s = this.Insert(GZTiJiao);
+                return s;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public IEnumerable<GZShenQing> GetAllApplicationsByName(string name)
+        {
+            try
+            {
+                IEnumerable<GZShenQing> AllApplicationsList =this.GetList(new { bgrxm = name }, null,false);
+                return AllApplicationsList;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
